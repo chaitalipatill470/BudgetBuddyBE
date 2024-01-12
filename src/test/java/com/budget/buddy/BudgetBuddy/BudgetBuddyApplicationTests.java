d30fee2f260da50305460d634d198fdeabfcb6c8
chaitalipatil4700 <68694992+chaitalipatil4700@users.noreply.github.com>
2024-01-12 10:43:04 +0100

    initial commit

---
+++ src/test/java/com/budget/buddy/BudgetBuddy/BudgetBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.budget.buddy.BudgetBuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
